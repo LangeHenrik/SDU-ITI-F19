8b7ed23e1a69dc5760f2b2ff8b96e63827e69dc1
Kraider <nipet17@student.sdu.dk>
2019-02-27 12:10:14 +0100

    Script
    Jeg synes din RegEx ser korrekt ud. (Y)
    Dog tror jeg det giver problemer hvis du laver en alert onSubmit, da den kun kan lave en alert ad gangen. Jeg anbefaler at benytte en omblur ved hvert enkelt input.

--- peten17/HomeworkForLesson2/script.js
+++ peten17/HomeworkForLesson2/script.js
@@ -11,31 +11,6 @@ function party() {
 }
 
 
-function checkInput() {
-  var name = document.getElementById('name').value;
-  var password = document.getElementById('password').value;
-  var phone = document.getElementById('phone').value;
-  var email = document.getElementById('email').value;
-  var zip = document.getElementById('zip').value;
-
-  var nameRE = /([a-z]|[A-Z])+\s+([a-z]|[A-Z])/g;
-
-  if (true) {
-
-  } else {
-
-  }
-
-
-  if (password.length < 8) {
-    alert("password too short. Please try againg.");
-    return false;
-
-  } else {
-    return true;
-  }
-
-
 
 
 
