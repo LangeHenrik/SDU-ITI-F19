7a79e5299a04944e8bcbb698f2e5e753ad930e78
Kraider <nipet17@student.sdu.dk>
2019-03-06 17:14:35 +0100

    Revert "Assignemt and login page"
    Det er super fedt du har fået php til at fungere.  Forstår dog ikke hvad isset skal bruges til?
    Og det ser ud som om du starter en session der hedder login, men kan ikke se den nogle steder.

--- peten17/AssignmentNo1/index.php
+++ peten17/AssignmentNo1/index.php
@@ -1,11 +1,4 @@
 
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-  session_start();
-}
-
-
-
 if (isset($_POST["submit"])) {
 if (isset($_POST['username'])  && isset($_POST['password']) ) {
   if ($_POST['username'] === "admin" && $_POST['password'] === "password") {
@@ -23,33 +16,3 @@ if (isset($_POST['username'])  && isset($_POST['password']) ) {
  ?>
 
 
-
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="indexStyle.css">
-  </head>
-  <body>
-    <div class="header">
-    <h1>Welcome to my website!</h1>
-    </div>
-    <div class="loginBox">
-      <form class="loginForm" method="post">
-        <fieldset>
-          <legend>Login</legend>
-          <p>Username:</p>
-          <input type="text" name="username" value="" id="username"> <br>
-          <p>Password:</p>
-          <input type="password" name="password" value="" id="password">
-          <input type="submit" name="submit" value="Submit">
-          <p>Not a member yet?</p> <a href="startpage.php">Click here to sign up!</a>
-        </fieldset>
-      </form>
-
-    </div>
-
-  </body>
-</html>
