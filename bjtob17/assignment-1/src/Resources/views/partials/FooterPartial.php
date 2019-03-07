2a190d0e3334f32c4b6a83af01c767c98c0caefe
bjarkt <bjarke.tobiesen@gmail.com>
2019-03-07 18:38:07 +0100

    okay

--- bjtob17/assignment-1/src/Resources/views/partials/FooterPartial.php
+++ bjtob17/assignment-1/src/Resources/views/partials/FooterPartial.php
@@ -8,7 +8,19 @@ class FooterPartial
     public static function show()
     {
         echo <<<EOL
-<footer class="main-footer">The footer</footer>
+<footer class="main-footer">
+<div class="column">
+<p class="title">Contact:</p>
+<p>admin@photowebsite.com</p>
+</div>
+<div class="column">
+<p class="title">Adresse:</p>
+<p>Nicevej 69, Odense 5000</p>
+</div>
+<div class="column">
+Tredje kolonne
+</div>
+</footer>
 </div>
 
 <script src="/Resources/js/js.js"></script>
