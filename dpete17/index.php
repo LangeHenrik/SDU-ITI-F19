890da3fd6d0a9863f6f0eb993149f5fb73d794de
Dennis Petersen <dennis-drengen@hotmail.dk>
2019-03-07 23:59:49 +0100

    Structure of HTML

---
+++ dpete17/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+     crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Project A01</title>
+</head>
+<body>
+    <div class="container">
+        <header class="main-header">
+            <h1>Project A01</h1>
+        </header>
+        <div class="sign-container">
+            <div class="sign-action">
+                <div><a href="">Register</a></div>
+                <div><a href="">Login</a></div>
+            </div>
+            <div class="sign-content">
+                <form method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" />
+                    <label for="password">Password</label>
+                    <input type="password" name="password" />
+                    <label for="firstname">First Name</label>
+                    <input type="text" name="firstname" />
+                    <label for="lastname">Last Name</label>
+                    <input type="text" name="lastname" />
+                </form>
+            </div>
+        </div>
+        <footer class="main-footer">
+            <div class="footer-aboutus">
+                <h3>About Us</h3>
+                <ul>
+                    <li><a href="#">Category 1, 1</a></li>
+                    <li><a href="#">Category 1, 2</a></li>
+                    <li><a href="#">Category 1, 3</a></li>
+                    <li><a href="#">Category 1, 4</a></li>
+                </ul>
+            </div>
+            <div class="footer-usefullinks">
+                <h3>Useful Links</h3>
+                <ul>
+                    <li><a href="#">Category 2, 1</a></li>
+                    <li><a href="#">Category 2, 2</a></li>
+                    <li><a href="#">Category 2, 3</a></li>
+                    <li><a href="#">Category 2, 4</a></li>
+                </ul>
+            </div>
+            <div class="footer-contact">
+                <h3>Contact</h3>
+                <div>
+                    <div>+45 88 88 88 88</div>
+                    <br>
+                    <div>web@teknologi.com</div>
+                    <br>
+                    <div>
+                        Localhost <br>
+                        The Internet <br>
+                        The Earth
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
