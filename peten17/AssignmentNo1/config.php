45cdc9c48dab3cf019bf60185579f5604afe30ef
peten17 <peten17@users.noreply.github.com>
2019-03-08 11:14:30 +0100

    register form added

--- peten17/AssignmentNo1/config.php
+++ peten17/AssignmentNo1/config.php
@@ -1,30 +1,13 @@
 <?php
 
-// //phpinfo();
-
-$servername = "localhost";
-$username = "root";
-$password = "1234";
-$dbname = "loginsys";
+include 'DB_config.php';
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SHOW databases;");
-    $stmt->execute();
 
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $result = $stmt->fetchAll();
-
-    print_r($result);
-
-  //  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-      //  echo $v;
-  //    }
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
-$conn = null;
 ?>
