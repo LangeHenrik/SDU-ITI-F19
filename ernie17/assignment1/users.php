e10faa47fae4c7ceb01b59f23b97a637bda52bdc
Unknown <erik@hagebjerg.dk>
2019-03-19 11:56:35 +0100

    Intial user page

--- ernie17/assignment1/users.php
+++ ernie17/assignment1/users.php
@@ -1,1 +1,37 @@
+<?php
+    # Starty session
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    
+    # establish db connection
+    require_once 'db_config.php';
+
+    try {
+
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname",
+        $username,
+        $password,
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+        $stmtGetUsers = $conn->prepare("SELECT username, firstname, lastname FROM picture_user");
+
+        $stmtGetUsers->execute();
+        $stmtGetUsers->setFetchMode(PDO::FETCH_ASSOC);
+        $resultGetUsers = $stmtGetUsers->fetchAll();
+        print_r($resultGetUsers);
+
+    } catch (PDOexception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+
+
+
+
+	# Close db connection
+	$conn = null;
+?>
+
 <h1>Users!</h1>
