f14cdbb2d5064a2607c34de85473bb458c7b52af
Erik <erik@hagebjerg.dk>
2019-03-20 14:11:25 +0100

    Fixed header buttons

--- ernie17/assignment1/footer.php
+++ ernie17/assignment1/footer.php
@@ -1,6 +1,5 @@
 <footer>
     <div class="footer">
-        <!-- <a href="pictures.php" id="btn-logout">Logout</a> -->
         <form method='post'>
             <button name='logout' id="btn-logout">Log out</button>
         </form>
