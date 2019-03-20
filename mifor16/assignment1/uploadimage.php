cf9e3c60d6764c6b3ff24356e50ed3438b8af5f6
mforberg <mifor16@student.sdu.dk>
2019-03-20 14:11:18 +0100

    upload image page begun

--- mifor16/assignment1/uploadimage.php
+++ mifor16/assignment1/uploadimage.php
@@ -1,23 +1,25 @@
 <?php
-error_reporting(E_ALL);
-session_start();
-if (!isset($_SESSION['login_user'])) {
-    header("location: login.php");
-}
+
+
+
 ?>
 
 <html>
+
 <head>
-    <title>Main Feed</title>
+    <title>Upload Images</title>
     <link href="mystylesheet.css" type="text/css" rel="stylesheet">
 </head>
+
 <body>
-<h1>Index</h1>
+<h1>Upload Image</h1>
 <nav id="nav">
     <a href="index.php">INDEX</a>
     <a href="users.php">USERS</a>
     <a href="uploadimage.php">UPLOAD</a>
     <a href="logout.php">LOGOUT</a>
-</nav>
+</nav><br><br>
+
 </body>
+
 </html>
--- mifor16/assignment1/users.php
+++ mifor16/assignment1/users.php
@@ -19,6 +19,7 @@
 <nav id="nav">
     <a href="index.php">INDEX</a>
     <a href="users.php">USERS</a>
+    <a href="uploadimage.php">UPLOAD</a>
     <a href="logout.php">LOGOUT</a>
 </nav>
 <br><br><br>
