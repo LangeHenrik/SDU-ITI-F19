3569a5447d8fd72819f31cff82542107c40e2ee7
Matias Marek <matias_marek@icloud.com>
2019-03-21 09:26:09 +0100

    deleted all previous files, and moved everything to php files. Added following functionality: * Register user * Login * Logout * Reset Password * Upload picture 	Needs CSS makeover, and for some reason there is a extra button that should be removed. 	saves to upload folder, but does not save to db * Picture gallery 	Shows static image gallery, does not yet show the dynamic number of uploaded pictures.

---
+++ upload.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matiasmarek
+ * Date: 20/03/2019
+ * Time: 19.29
+ */
+
+$target_dir = "picture/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+function goToUploadPage() {
+    header('Location: welcome.php');
+}
+
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+
+        goToUploadPage();
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .button {
+            display: inline-block;
+            border-radius: 4px;
+            background-color: #f4511e;
+            border: none;
+            color: #FFFFFF;
+            text-align: center;
+            font-size: 28px;
+            padding: 20px;
+            width: 200px;
+            transition: all 0.5s;
+            cursor: pointer;
+            margin: 5px;
+        }
+
+        .button span {
+            cursor: pointer;
+            display: inline-block;
+            position: relative;
+            transition: 0.5s;
+        }
+
+        .button span:after {
+            content: '\00bb';
+            position: absolute;
+            opacity: 0;
+            top: 0;
+            right: -20px;
+            transition: 0.5s;
+        }
+
+        .button:hover span {
+            padding-right: 25px;
+        }
+
+        .button:hover span:after {
+            opacity: 1;
+            right: 0;
+        }
+    </style>
+</head>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+
+    <h1> Select image to upload: </h1>
+    <input class="button" type="file" name="fileToUpload"  id="fileToUpload">
+    <input class="button" type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
