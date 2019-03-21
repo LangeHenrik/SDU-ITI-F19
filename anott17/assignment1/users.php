400568a410346728b509455def21d81080bef5f9
Anders Bensen Ottsen <anders_bensen@hotmail.com>
2019-03-21 21:30:03 +0100

    Assignment 1 finished
    This is hopefully the final version, all requirements are now fullfilled, with a migration file and a folder with pictures of all the pages.

--- anott17/assignment1/users.php
+++ anott17/assignment1/users.php
@@ -1,5 +1,18 @@
 <?php
 
+  session_start();
+
+  if (!(isset($_SESSION['Login'])) || $_SESSION['Login'] !== true) {
+    echo 'You are not logged in.';
+    return;
+  }
+
+  if (isset(($_POST["logout"]))) {
+    header('Location: index.php');
+    $_SESSION['Login'] = false;
+    session_destroy();
+  }
+
   require_once 'db_config.php';
   try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname",
@@ -18,19 +31,6 @@
   $getUsersStmt->setFetchMode(PDO::FETCH_ASSOC);
   $result = $getUsersStmt->fetchAll();
 
-
-  session_start();
-
-  if (!(isset($_SESSION['Login'])) || $_SESSION['Login'] !== true) {
-    echo 'You are not logged in.';
-    return;
-  }
-
-  if (isset(($_POST["logout"]))) {
-    header('Location: index.php');
-    $_SESSION['Login'] = false;
-    session_destroy();
-  }
 ?>
 
 <!DOCTYPE html>
@@ -39,6 +39,31 @@
     <meta charset="utf-8">
     <link rel ="stylesheet" type="text/css" href="style.css">
     <title></title>
+    <script>
+    function showUser(str) {
+        if (str == "") {
+            console.log(str);
+            document.getElementById("searchResults").innerHTML = " ";
+            return;
+        } else {
+            console.log(str);
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                // code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("searchResults").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET","getUserName.php?q="+str,true);
+            xmlhttp.send();
+        }
+    }
+    </script>
   </head>
   <body>
     <div class="pictureDiv">
@@ -62,6 +87,18 @@
       </a>
     </div>
     <div class="usersTableDiv">
+      <div class="headerDiv">Here you can search for a specific user name for more information:</div>
+      <form class = "searchForUsersForm">
+        User name: <input type="text" onkeyup="showUser(this.value)">
+      </form>
+      <div id="searchResults">
+
+      </div>
+
+    </div>
+    <div class="usersTableDiv">
+      <!-- <h2>Here is a list of all users:</h2> -->
+      <div class="headerDiv">Here is a list of all users:</div>
       <table class="usersTable">
         <th>Username</th>
         <th>Frontname</th>
