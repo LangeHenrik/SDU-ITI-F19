1b428698c92c3fa4875762d52e1787cb30dfa522
Mathias Tværmose Gleerup <Mathiastgleerup@gmail.com>
2019-03-22 19:36:46 +0100

    updated migration.sql

--- magle17/assignment1/migration.sql
+++ magle17/assignment1/migration.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.2.14-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2019-03-22 16:13:38
+-- Date/time:                    2019-03-22 19:35:18
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `media` (
   PRIMARY KEY (`id`),
   KEY `uploaded_by` (`uploaded_by`),
   CONSTRAINT `media_ibfk_1` FOREIGN KEY (`uploaded_by`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table magle17.media: ~37 rows (approximately)
 /*!40000 ALTER TABLE `media` DISABLE KEYS */;
@@ -34,14 +34,13 @@ INSERT INTO `media` (`id`, `uploaded_by`, `media_name`, `title`, `description`) 
 	(4, 9, '1553188817-download.jpg', 'Wood hut', 'Sikke en bl&aelig;ret hytte!'),
 	(5, 9, '1553188847-carwithextra.jpg', 'Mega bl&aelig;ret vogn', 'WOW nogle p&aelig;ne forlygter'),
 	(6, 9, '1553188880-car.jpg', 'Bl&aelig;rede hjul!', 'De her hjul er sindssygt bl&aelig;rede!'),
-	(7, 9, '1553188908-picture.jpg', 'Bl&aelig;ret hus!', 'Det her er et bl&aelig;ret hus!'),
 	(8, 9, '1553188928-picture1.jpg', 'Bl&aelig;ret pool!', 'Det her er en bl&aelig;ret pool!'),
 	(9, 9, '1553188956-picture2.jpg', 'Bl&aelig;ret nok sofa i guess', 'MEGA fucking bl&aelig;ret sofa!'),
 	(10, 9, '1553189167-pimpmywhat.jpg', 'Meme team', 'fordi det lige passede ind'),
 	(11, 9, '1553189457-maxresdefault.jpg', 'FASAN', 'fasaner er fandme fine'),
 	(12, 9, '1553189485-tukan2.jpg', 'Tukan', 'Tukaner er truncated'),
 	(13, 9, '1553189527-tukan.jpg', '2 Tukaner', '2 tukaner er dobbelt s&aring; bl&aelig;ret!'),
-	(14, 9, '1553189659-dinosaur.jpg', 'Det en fuckin&amp;#39; dinosaur!', 'Gas. det bar et bl&aelig;ret n&aelig;sehorn.'),
+	(14, 9, '1553189659-dinosaur.jpg', 'Det en fucking dinosaur!', 'Gas. det bar et bl&aelig;ret n&aelig;sehorn.'),
 	(15, 9, '1553189726-vulkan.jpg', 'vulkan!', 'Vulkaner er ogs&aring; bl&aelig;rede!'),
 	(16, 9, '1553193966-vinikg.jpg', 'Viking Bl&aelig;rer Sig Med ULV!', 'Bl&aelig;ret ud og en vanvittig viking'),
 	(17, 9, '1553194010-images2.png', 'Jesus er bl&aelig;ret til p&aring;ske', 'P&aring;ske&aelig;g er rimelig bl&aelig;ret. Jesus d&oslash;de i p&aring;sken.'),
@@ -65,8 +64,9 @@ INSERT INTO `media` (`id`, `uploaded_by`, `media_name`, `title`, `description`) 
 	(35, 9, '1553217659-getsdf.jpg', 'kjsdnfkjsd', 'sljfnlsdfnsdf'),
 	(36, 9, '1553217674-gladiator.jpg', 'FUCKING KRIGER TING', 'AKSDNFNDJANJFNSDFNASDKFNKASNDFNASDFKSDJKNF'),
 	(37, 9, '1553217686-images.jpg', 'DET JOSDKFMSKDFMSKDF', 'SKMDFSDFLSDKFMSKLDFMSKLDF'),
-	(38, 9, '1553217698-penguin.jpg', 'LINUX', 'LNUX'),
-	(39, 9, '1553217710-piskflÃ¸de.jpg', 'pisk mig i fl&oslash;den', 'lsjkadnfjksndflks');
+	(40, 9, '1553273778-penguin.jpg', 'LINUX PINGVIN', 'PINGVIN PINGVIN PINGVIN PINGVIN PINGVIN PINGVIN PINGVIN PINGVIN PINGVIN PINGVIN '),
+	(41, 9, '1553273803-piskflÃ¸de.jpg', 'DANSK PISKEFL&Oslash;DE', 'Bl&aelig;ret nok med piskefl&oslash;de!!!!!'),
+	(42, 9, '1553278570-1553188908-picture.jpg', 'Bl&aelig;ret hus', 'bl&aelig;ret hus uploadet efter jeg kom til at slette det');
 /*!40000 ALTER TABLE `media` ENABLE KEYS */;
 
 
@@ -83,12 +83,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` tinytext NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
--- Dumping data for table magle17.users: ~1 rows (approximately)
+-- Dumping data for table magle17.users: ~3 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `username`, `firstname`, `lastname`, `zip`, `city`, `email`, `phone`, `password`) VALUES
-	(9, 'mathiastg', 'Mathias', 'Gleerup', '5000', 'Odense', 'mathiastgleerup@gmail.com', '+26715958', 'ygtf0921');
+	(9, 'magle17', 'Mathias', 'Gleerup', '5000', 'Odense', 'template@domain.com', '+88888888', 'root1234'),
+	(10, 'bruger2', 'lars', 'larsen', '2800', 'lyngby', 'test@domain.com', '+895684521', 'bruger2'),
+	(11, 'nybruger', 'thomas', 'thomsen', '5700', 'svendborg', 'test2@domain.com', '+342345345', 'nybruger');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
