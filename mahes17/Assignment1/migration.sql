9a52ed64210713b2bc464cdc021ea9867f4c6aed
hrmalte <maltehesk@hotmail.com>
2019-03-22 14:20:42 +0100

    INSERT info

--- mahes17/Assignment1/migration.sql
+++ mahes17/Assignment1/migration.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.2.14-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2019-03-22 08:20:17
+-- Date/time:                    2019-03-22 12:58:25
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,23 +28,34 @@ CREATE TABLE IF NOT EXISTS `person` (
   `email` varchar(100) DEFAULT NULL,
   `phoneNumber` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`person_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table mahes17.person: ~4 rows (approximately)
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+INSERT INTO `person` (`person_id`, `firstName`, `userPassword`, `lastName`, `zipcode`, `city`, `email`, `phoneNumber`) VALUES
+	(10, 'Patrik', '$2y$10$614V.aCgvRe5wVyU4o6dce/jtPt9jMYorR/0xFUikcBNgegwKNMGy', 'Star', 12332, 'Bikini Bottom', 'patrick@seea.com', 1233221),
+	(11, 'Malte', '$2y$2y$10$GT56vZJVHQhV2XEieWe/Ze1Ca5/nhHsimsBlz1zmungFSS05nOcK2', 'Hesk', 5200, 'Odense', 'maltehesk@hotmail.com', 24877134),
+	(12, 'Patrick', '$2y$10$IR2IRmd1f7r1GyLtvmlf0OUu.Dht2pWKpQvfkAYujN1ZtkzOygal.', 'Star', 2878, 'Bikini Bottom', 'patrick@seea.com', 32343),
+	(13, 'Tim', '$2y$10$vfH1kPAiTxpvkgou0dknSuakYVUyk.kr5pFyx1laHnDMO1x2dAr0.', 'TIm', 2132, 'dsfsdfds', 'tim@sdasd.com', 12323333);
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
 
 
 -- Dumping structure for table mahes17.picture
 DROP TABLE IF EXISTS `picture`;
 CREATE TABLE IF NOT EXISTS `picture` (
   `picture_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `pictureImage` varchar(100) NOT NULL,
+  `pictureImage` varchar(200) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `desc` varchar(200) NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`picture_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `picture_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `person` (`person_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table mahes17.picture: ~0 rows (approximately)
+/*!40000 ALTER TABLE `picture` DISABLE KEYS */;
+/*!40000 ALTER TABLE `picture` ENABLE KEYS */;
 
 
 -- Dumping structure for table mahes17.timelog
@@ -55,8 +66,13 @@ CREATE TABLE IF NOT EXISTS `timelog` (
   `eventTimestamp` varchar(100) NOT NULL,
   `responsible` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`timelog_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table mahes17.timelog: ~2 rows (approximately)
+/*!40000 ALTER TABLE `timelog` DISABLE KEYS */;
+INSERT INTO `timelog` (`timelog_id`, `eventName`, `eventTimestamp`, `responsible`) VALUES
+	(1, 'event', 'timestamp', 'responsible'),
+	(2, 'cool', '2019-03-21 01:13:51', 'Testuser.com');
+/*!40000 ALTER TABLE `timelog` ENABLE KEYS */;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
