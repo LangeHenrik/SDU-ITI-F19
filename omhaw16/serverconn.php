93707298d89b512b33b897632b5606fce1795599
Omar <omarnabilhawwash@gmail.com>
2019-03-22 15:42:26 +0100

    Rmoved residual comments - FINAL PUSH
    Assignment 1 done.

--- omhaw16/serverconn.php
+++ omhaw16/serverconn.php
@@ -3,16 +3,11 @@
     $connName   = "root";
     $connPass   = "1608";
     $dbname     = "itiproj";
-    // $port = "8889"
-    // Create connection
+    
     $conn = new mysqli($serverName, $connName, $connPass, $dbname);
 
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } else {   
     }
-
-    //Close the connection when u want
-    //$conn->close();
 ?>
