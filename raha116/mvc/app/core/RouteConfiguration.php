53beafad9a259ff37321cb0aeb672b8173db4fb5
zlepper <hansen13579@gmail.com>
2019-04-25 17:13:40 +0200

    Refactor stuff to work with the new strange paths

---
+++ raha116/mvc/app/core/RouteConfiguration.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace core;
+
+
+use controllers\CommentController;
+use controllers\FeedController;
+use controllers\ImageController;
+use controllers\TestController;
+use controllers\UserController;
+use framework\RequestResolver;
+
+class RouteConfiguration
+{
+    /**
+     * @var RequestResolver
+     */
+    private $resolver;
+
+    public function __construct(RequestResolver $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    public function configure()
+    {
+
+        // Configure user routes
+        $this->resolver->register_handler("GET", "/api/user", UserController::class, "index");
+        $this->resolver->register_handler("POST", "/api/user", UserController::class, "post");
+        $this->resolver->register_handler("POST", "/api/user/login", UserController::class, "login");
+        $this->resolver->register_handler("POST", "/api/user/logout", UserController::class, "logout");
+        $this->resolver->register_handler("GET", "/api/user/isLoggedIn", UserController::class, "isLoggedIn");
+
+        // Configure feed routes
+        $this->resolver->register_handler("GET", "/api/feed", FeedController::class, "index");
+        $this->resolver->register_handler("POST", "/api/feed", FeedController::class, "post");
+        $this->resolver->register_handler("DELETE", "/api/feed/(?<id>\d+)", FeedController::class, "delete");
+
+        // Configure image routes
+        $this->resolver->register_handler("GET", "/api/image/(?<id>\d+)", ImageController::class, "get");
+
+        // Configure comment routes
+        $this->resolver->register_handler("POST", "/api/comment", CommentController::class, "post");
+
+        $this->resolver->register_handler("GET", "/api/test/(?<foo>\w+)/(?<bar>\w)", TestController::class, "test");
+
+    }
+}
---
+++ raha116/mvc/app/services/ImageService.php
@@ -0,0 +1,119 @@
+<?php
+
+
+namespace services;
+
+
+use models\ImageDatabaseEntry;
+use models\UploadedFile;
+use repositories\ImageRepository;
+use utilities\IO;
+
+class ImageService
+{
+    private $STORAGE_PATH;
+
+    /**
+     * @var ImageRepository
+     */
+    private $imageRepository;
+
+    /**
+     * ImageService constructor.
+     * @param ImageRepository $imageRepository
+     */
+    public function __construct(ImageRepository $imageRepository)
+    {
+        $this->imageRepository = $imageRepository;
+
+        // Ensure output directory exists
+        $this->STORAGE_PATH = IO::join_paths(__DIR__, "..", "storage");
+        if (!file_exists($this->STORAGE_PATH)) {
+            mkdir($this->STORAGE_PATH);
+        }
+    }
+
+    public function save_image(UploadedFile $image): ImageDatabaseEntry
+    {
+        $hash = $this->get_image_hash($image);
+
+        // Check if the image has been uploaded before. If it has, we can just reuse it.
+        $image_data = $this->imageRepository->get_image_from_filehash($hash);
+        if ($image_data) {
+            return $image_data;
+        }
+
+        $extension = explode("/", $image->type)[1];
+
+        $output_path = IO::join_paths($this->STORAGE_PATH, "$hash.$extension");
+
+        move_uploaded_file($image->tmp_name, $output_path);
+
+        return $this->imageRepository->add_image($hash, $extension);
+    }
+
+    private function get_image_hash(UploadedFile $image)
+    {
+        return hash_file("sha256", $image->tmp_name);
+    }
+
+
+    /**
+     * @param int $image_id
+     * @return ImageDatabaseEntry|null
+     */
+    public function get_image_entry(int $image_id)
+    {
+        return $this->imageRepository->get_image($image_id);
+    }
+
+    /**
+     * Deletes the specified image
+     *
+     * @param int $image_id
+     */
+    public function delete_image(int $image_id)
+    {
+        $entry = $this->get_image_entry($image_id);
+
+        unlink(IO::join_paths($this->STORAGE_PATH, $entry->get_filename()));
+
+        $this->imageRepository->delete_image($image_id);
+    }
+
+    /**
+     * Prints the refered image to stdout
+     *
+     * @param int $image_id
+     *
+     * @return bool true if the image exists, false otherwise
+     */
+    public function print_image(int $image_id): bool
+    {
+        $entry = $this->imageRepository->get_image($image_id);
+
+        if (!$entry) {
+            return false;
+        }
+
+        $image_path = IO::join_paths($this->STORAGE_PATH, $entry->get_filename());
+
+        $image_info = @getimagesize($image_path);
+
+        if (!$image_info) {
+            error_log("Attempted to load image that didn't exist on disk for some reason????");
+            return false;
+        }
+
+        error_log("Image info is valid");
+        error_log(json_encode($image_info));
+
+        header("Content-Type: " . $image_info["mime"]);
+        header("Content-Length: " . filesize($image_path));
+
+        readfile($image_path);
+
+        return true;
+    }
+
+}
