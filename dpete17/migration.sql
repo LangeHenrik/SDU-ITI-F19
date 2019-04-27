0400239fed6f16ced046ddf50020803a81968184
Dennis Petersen <dennis-drengen@hotmail.dk>
2019-04-27 14:29:06 +0200

    Updated migration.sql

--- dpete17/migration.sql
+++ dpete17/migration.sql
@@ -18,7 +18,7 @@ CREATE TABLE account (
 
 CREATE TABLE image (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    filename VARCHAR(100) UNIQUE NOT NULL,
+    file LONGBLOB NOT NULL,
     header VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
     uploaded_at TIMESTAMP NOT NULL DEFAULT now()
@@ -37,35 +37,31 @@ CREATE TABLE uploads (
     PRIMARY KEY (account_id, image_id)
 );
 
--- Password is Admin1
 INSERT INTO account(username, password, firstname, lastname, zip, city, email, phone)
-VALUES ('Admin1', '$2y$10$GHn7SOuZltfGtzGl5WnN/.ksQxLZIQf7zCksNnUbjV8hB5QoLb4V2', 'Admin', 'Nimad', 6000, 'Admincity', 'admin@admin.com', 99999999);
+VALUES ('admin1', '$2y$10$GHn7SOuZltfGtzGl5WnN/.ksQxLZIQf7zCksNnUbjV8hB5QoLb4V2', 'Admin', 'Nimad', 6000, 'Admincity', 'admin@admin.com', 99999999);
 
--- Password is Testo0
 INSERT INTO account(username, password, firstname, lastname, zip, city, email, phone)
-VALUES ('Testo1', '$2y$10$UnJB5PYBLmNCjbtLj2jQMee1hGCqw2xOoI2JnywAdsbQkkhJicmKO', 'Tester', 'Retset', 6000, 'Testercity', 'tester1@tester.com', 00000001);
+VALUES ('testo1', '$2y$10$UnJB5PYBLmNCjbtLj2jQMee1hGCqw2xOoI2JnywAdsbQkkhJicmKO', 'Tester', 'Retset', 6000, 'Testercity', 'tester1@tester.com', 00000001);
 
--- Password is Testo0
 INSERT INTO account(username, password, firstname, lastname, zip, city, email, phone)
-VALUES ('Testo2', '$2y$10$UnJB5PYBLmNCjbtLj2jQMee1hGCqw2xOoI2JnywAdsbQkkhJicmKO', 'Tester', 'Retset', 6000, 'Testercity', 'tester2@tester.com', 00000002);
+VALUES ('testo2', '$2y$10$UnJB5PYBLmNCjbtLj2jQMee1hGCqw2xOoI2JnywAdsbQkkhJicmKO', 'Tester', 'Retset', 6000, 'Testercity', 'tester2@tester.com', 00000002);
 
--- Password is Testo0
 INSERT INTO account(username, password, firstname, lastname, zip, city, email, phone)
 VALUES ('dpete17', '$2y$10$UnJB5PYBLmNCjbtLj2jQMee1hGCqw2xOoI2JnywAdsbQkkhJicmKO', 'Dennis', 'Unknown', 6000, 'Odense M', 'creator@htmlgod.com', 35700540);
 
-INSERT INTO image(filename, header, content) VALUES ('55a86c4f1ae2ae842081b6be159018774c8c1823295d0fdeb9c4849c684257c3', 'New Opinion System!', '');
-INSERT INTO image(filename, header, content) VALUES ('c484c50881fd73684ba154879d16d28470acf4b2aba83ac22d48235829f5a7d8', 'A Star!', '');
-INSERT INTO image(filename, header, content) VALUES ('0f942abf332a0e9d06e7535308070228c23717ffeeba8f7d8d8be43d489776db', 'SDU LOGO', '');
-INSERT INTO image(filename, header, content) VALUES ('0b922a1b3cd5ff16ea813736b1d892357f5910821c93e9a7092bcc39a47785b2', 'A Penguin!', 'My first penguin!');
-
-INSERT INTO uploads(account_id, image_id) VALUES (1, 1);
-INSERT INTO uploads(account_id, image_id) VALUES (1, 2);
-INSERT INTO uploads(account_id, image_id) VALUES (2, 3);
-INSERT INTO uploads(account_id, image_id) VALUES (4, 4);
-
-INSERT INTO opinion(account_id, image_id, opinion) VALUES (1, 1, 'LIKES');
-INSERT INTO opinion(account_id, image_id, opinion) VALUES (1, 3, 'LIKES');
-INSERT INTO opinion(account_id, image_id, opinion) VALUES (2, 2, 'LIKES');
-INSERT INTO opinion(account_id, image_id, opinion) VALUES (2, 4, 'LIKES');
-INSERT INTO opinion(account_id, image_id, opinion) VALUES (3, 3, 'LIKES');
-INSERT INTO opinion(account_id, image_id, opinion) VALUES (3, 4, 'LIKES');
+# INSERT INTO image(filename, header, content) VALUES ('55a86c4f1ae2ae842081b6be159018774c8c1823295d0fdeb9c4849c684257c3', 'New Opinion System!', '');
+# INSERT INTO image(filename, header, content) VALUES ('c484c50881fd73684ba154879d16d28470acf4b2aba83ac22d48235829f5a7d8', 'A Star!', '');
+# INSERT INTO image(filename, header, content) VALUES ('0f942abf332a0e9d06e7535308070228c23717ffeeba8f7d8d8be43d489776db', 'SDU LOGO', '');
+# INSERT INTO image(filename, header, content) VALUES ('0b922a1b3cd5ff16ea813736b1d892357f5910821c93e9a7092bcc39a47785b2', 'A Penguin!', 'My first penguin!');
+#
+# INSERT INTO uploads(account_id, image_id) VALUES (1, 1);
+# INSERT INTO uploads(account_id, image_id) VALUES (1, 2);
+# INSERT INTO uploads(account_id, image_id) VALUES (2, 3);
+# INSERT INTO uploads(account_id, image_id) VALUES (4, 4);
+#
+# INSERT INTO opinion(account_id, image_id, opinion) VALUES (1, 1, 'LIKES');
+# INSERT INTO opinion(account_id, image_id, opinion) VALUES (1, 3, 'LIKES');
+# INSERT INTO opinion(account_id, image_id, opinion) VALUES (2, 2, 'LIKES');
+# INSERT INTO opinion(account_id, image_id, opinion) VALUES (2, 4, 'LIKES');
+# INSERT INTO opinion(account_id, image_id, opinion) VALUES (3, 3, 'LIKES');
+# INSERT INTO opinion(account_id, image_id, opinion) VALUES (3, 4, 'LIKES');
