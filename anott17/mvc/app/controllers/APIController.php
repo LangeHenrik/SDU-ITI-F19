85371c59e9855f200831b0ee70406d24d8fe1ecd
Anders Bensen Ottsen <anders_bensen@hotmail.com>
2019-04-30 09:20:40 +0200

    Assignment2
    ASsignment 2 with all tests passed

---
+++ anott17/mvc/app/controllers/APIController.php
@@ -0,0 +1,59 @@
+<?php
+
+class APIController extends Controller {
+
+	public function index () {
+	}
+
+	public function users () {
+		$users = $this->model('User') -> getUserNamesAndIDs();
+
+		$json_users = json_encode($users, JSON_PRETTY_PRINT);
+		header("Content-Type:application/json");
+		echo $json_users;
+	}
+
+	public function pictures ($user, $userID) {
+		// print_r($_SERVER['REQUEST_METHOD']);
+		if (isset($_SERVER['REQUEST_METHOD']) && ($_SERVER['REQUEST_METHOD'] === 'POST')) {
+			//$input = $_POST['json'];
+
+			$input = json_decode($_POST['json'], true);
+			$imageBlob = $input['image'];
+			$title = $input['title'];
+			$description = $input['description'];
+			$username = $input['username'];
+			$password = $input['password'];
+
+			$returnedUserID = $this->model('User') -> validateUsers($username, $password);
+			//print_r($returnedUserID);
+			//print('as');
+			//print_r($userID);
+			if ($returnedUserID == $userID) {
+				// print('asasdasdasdsdas');
+				$image_id = $this->model('Picture') -> APIUploadPicture($imageBlob, $title, $description, $userID);
+
+				$image_id = array('image_id' => $image_id);
+
+				$json_users = json_encode($image_id, JSON_PRETTY_PRINT);
+				header("Content-Type:application/json");
+				echo $json_users;
+			}
+		} else if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'GET'){
+			// print('asasdasdasdsdas');
+			$pictures = $this->model('Picture') -> getPicturesFromUser($userID);
+
+			$returnedObject = array();
+
+			foreach ($pictures as $picture) {
+				$returnedObject[] = array($picture);
+				// code...
+			}
+
+			$json_users = json_encode($pictures, JSON_PRETTY_PRINT);
+
+			header("Content-Type:application/json");
+			echo $json_users;
+		}
+	}
+}
--- anott17/mvc/app/views/home/index.php
+++ anott17/mvc/app/views/home/index.php
@@ -1,3 +1,89 @@
-<?php include '../app/views/partials/menu.php'; ?>
+<?php include '../app/views/partials/head.php'; ?>
 
-Hello there, <?=$viewbag['username']?>
+<div class="mainDiv">
+  <h1>Welcome to Anott17's website</h1>
+  <div class="registerDiv">
+    <div class="registerCheckDiv">
+      <form class="form-register" onsubmit="return checkFields()" method="post" action="home/register">
+        <fieldset class="fieldset-register">
+          <legend>Register</legend>
+          <label for="userName" style="color: black">User name</label>
+          <br>
+          <input type="text" name="userName" id="userName" onkeyup="checkFields()" >
+          <br>
+
+          <label for="password">Password</label>
+          <br>
+          <input type="password" name="password" id="password" onkeyup="checkFields()" >
+          <br>
+
+          <label for="secondPassword">Reenter password</label>
+          <br>
+          <input type="password" name="secondPassword" id="secondPassword" onkeyup="checkFields()" >
+          <br>
+
+          <label for="frontName" style="color: black">Front name</label>
+          <br>
+          <input type="text" name="frontName" id="frontName" onkeyup="checkFields()" >
+          <br>
+
+          <label for="lastName" style="color: black">Last name</label>
+          <br>
+          <input type="text" name="lastName" id="lastName" onkeyup="checkFields()" >
+          <br>
+
+          <label for="zip">Zip code</label>
+          <br>
+          <input type="text" name="zip" id="zip" onkeyup="checkFields()" >
+          <br>
+
+          <label for="city">City</label>
+          <br>
+          <input type="text" name="city" id="city" onkeyup="checkFields()" >
+          <br>
+
+          <label for="phone">Phone number</label>
+          <br>
+          <input type="text" name="phone" id="phone" onkeyup="checkFields()" >
+          <br>
+
+          <label for="email">Email adress</label>
+          <br>
+          <input type="text" name="email" id="email" onkeyup="checkFields()" >
+          <br>
+
+          <input type="submit" value="Register" name="submit" class="mainButton">
+
+          <div id ="errorDiv" class="registerErrorDiv">
+            <p></p>
+            <?php echo''.$_SESSION['globalRegisterMsg'];
+            $_SESSION['globalRegisterMsg'] = ' ';?>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  </div>
+
+  <div class="loginDiv">
+    <form class="form-login" method="post" action="home/login">
+      <fieldset class="fieldset-login">
+        <legend>Login</legend>
+        <label for="userName" style="color: black">User name</label>
+        <br>
+        <input type="text" name="userNameLogin" id="userNameLogin">
+        <br>
+
+        <label for="password" style="color: black">Password</label>
+        <br>
+        <input type="password" name="passwordLogin" id ="passwordLogin">
+        <br>
+
+        <input type="submit" value="Login" class="mainButton">
+        <div id ="errorDivLogin" class="loginErrorDiv">
+          <?php echo''.$_SESSION['globalLoginMsg'];
+          $_SESSION['globalLoginMsg'] = ' ';?>
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</div>
