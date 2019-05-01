59dd8c368e5854fefdd592e7b742c6f26c1921bf
Stewil <32218463+Stewil@users.noreply.github.com>
2019-05-01 17:06:25 +0200

    Finished read users api. needs testing

--- jschr17/mvc/public/api/users/read.php
+++ jschr17/mvc/public/api/users/read.php
@@ -3,40 +3,43 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json, charset=UTF-8");
 
-//database connection
-$database = new Database;
-$db = $database->getConn();
 
-//create user array
-$users = array();
 
-//connect to database
-$getUsers = $db->prepare("SELECT * FROM users");
-$getusers->execute();
-//get list of users
-$userList = $getUsers->fetchAll();
-//print userlist to see what happens
-echo '<pre>'; echo print_r($userList); echo '</pre>';
-foreach ($userList as $usr) {
-    //for each tuple make new user object with the information
-    $user = new User($usr[0],$usr[1]);
-    //add user to array of users
-    array_push($users, $user);
+function read(){
+    //database connection
+    $database = new Database;
+    $db = $database->getConn();
+
+    //create user array
+    $users = array();
+
+    //connect to database
+    $query = $db->prepare("SELECT * FROM users");
+    $query->execute();
+
+    //get list of users
+    $userList = $query->fetchAll();
+    //check amount of users found
+    if ($userList->sizeof = 0){
+        http_response_code(404); //find correct response code
+        echo json_encode(array("message" => 'no users found.'));
+    } else {
+        //print userlist to see what happens
+        echo '<pre>'; echo print_r($userList); echo '</pre>';
+
+        foreach ($userList as $usr) {
+            //for each tuple make new user object with the information
+            $user = new User($usr[0],$usr[1]);
+            //add user to array of users
+            array_push($users, $user);
+        }
+        //json encode array of users and return it
+        $json = json_encode($users, JSON_PRETTY_PRINT);
+        http_response_code(200);
+        echo($json);
+        return($json);
+    }
 }
-
-
-//json encode array of users and return it
-$json = json_encode($users, JSON_PRETTY_PRINT);
-
-http_response_code(200);
-
-echo($json);
-
-
-/* TODO
-add checks for amount of users, set response based on check results
-add function to be called
- */
 
 //notes
 //json_encode($object)
