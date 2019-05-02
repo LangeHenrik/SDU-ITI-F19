3e58997d914fd9637afafbea93ba0264d4c5e5b9
Stewil <32218463+Stewil@users.noreply.github.com>
2019-05-02 17:15:56 +0200

    absolute path error in index

--- jschr17/mvc/public/index.php
+++ jschr17/mvc/public/index.php
@@ -9,12 +9,12 @@ require_once '../app/init.php';
 $router = new Router();
 
 //these two completely break the site: SyntaxError: JSON.parse: unexpected character at line 1 column 1 of the JSON data
-//include_once 'api/users/read.php';
-//include_once 'api/pictures/users/getimg.php';
+//include_once(__DIR__ . '/api/users/read.php');
+//include_once(__DIR__ . '/api/pictures/users/getimg.php';
 
 $router->add('/', ' ');
-$router->add('/api/users', read());
-$router->add('/api/pictures/user', read());
+$router->add('/api/users', 'read()');
+$router->add('/api/pictures/user', 'read()');
 
 echo '<pre>'; echo print_r($router); echo '</pre>';
 
