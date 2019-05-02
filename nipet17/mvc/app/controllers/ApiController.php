e52b97adbd661893cb8362cfe343ce0d357acc76
Kraider <nipet17@student.sdu.dk>
2019-05-02 18:56:01 +0200

    Sidste forsøg
    dss

--- nipet17/mvc/app/controllers/ApiController.php
+++ nipet17/mvc/app/controllers/ApiController.php
@@ -15,20 +15,27 @@ class ApiController extends Controller {
 
 
 	public function pictures($user, $userID) {
-		header("Content-Type: application/json");
 
-		$input 				= json_decode($_POST['json'],true);
-		$image 				= $input['image'];
-		$title 				= $input['title'];
-		$description	= $input['description'];
-		$username			=	$input['username'];
-		$password			= $input['password'];
+		//header("Content-Type: application/json");
 		if (isset($_SERVER['REQUEST_METHOD']) && ($_SERVER['REQUEST_METHOD'] === 'POST')) {
+			$input 				= json_decode($_POST['json'],true);
+			$image 				= $input['image'];
+			$title 				= $input['title'];
+			$description	= $input['description'];
+			$username			=	$input['username'];
+			$password			= $input['password'];
 
-			$image_id = $this->model('Api')->uploadPicture($image, $title, $description, $username, $password);
-			$image_id = array('image_id' => $image_id);
-			$json = json_encode($image_id, JSON_PRETTY_PRINT);
-			echo $json;
+			$returnedID = $this->model('User')->validateUser($username,$password);
+			if($returnedID ==$id) {
+				$image_id = $this->model('Api')->uploadPicture($image, $title, $description, $username);
+				$image_id = array('image_id' => $image_id);
+				$json = json_encode($image_id, JSON_PRETTY_PRINT);
+				echo $json;
+			} else {
+				$image_id = -1;
+				$image_id = array('image_id'=>$imageID);
+				$json = json_encode($imageID,JSON_PRETTY_PRINT);
+			}
 
 		} elseif (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'GET') {
 			$pictures = $this->model('Api') -> selectPictures($userID);
--- nipet17/mvc/app/models/Api.php
+++ nipet17/mvc/app/models/Api.php
@@ -1,6 +1,31 @@
 <?php
 class Api extends Database {
 
+	private function getUserNameFromID($id) {
+	$sql = $this->conn->prepare("SELECT login_username FROM login WHERE login_id = :userid");
+	$sql->bindparam(':userid', $id);
+	$sql->execute();
+	$sql->setFetchMode(PDO::FETCH_ASSOC);
+	$result = $sql->fetch();
+	$username = $result['login_username'];
+	return $username;
+	}
+
+	public function validateUser($username,$password){
+		 $sql = $this->conn->prepare("SELECT login_id, login_password FROM login WHERE login_username = :username");
+		 $sql->bindparam(':username', $username);
+		 $sql->execute();
+		 $sql->setFetchMode(PDO::FETCH_ASSOC);
+		 $result = $sql->fetch();
+		 if (password_verify($password, $result['login_password'])) {
+			 echo "approved";
+			 return $result['login_id'];
+		 } else {
+			 echo "not approved";
+			 return 'error';
+		 }
+	 }
+
 	 public function selectUsers() {
      $users = array();
      $sql   = "SELECT * FROM login ORDER BY login_id";
@@ -17,29 +42,28 @@ class Api extends Database {
    }
 
    public function uploadPicture($image, $title, $description, $username) {
-		 $username = $this->getUserNameFromID($userid);
-
-		$sql = $this->conn->prepare("INSERT INTO photo (photo_image, photo_text, photo_header, photo_user)
-									 VALUES (:image, :title, :description, :username)");
-		$sql->bindparam('image', $image);
-		$sql->bindparam('title', $title);
-		$sql->bindparam('description', $description);
-		$sql->bindparam('username', $username);
-		$sql->execute();
-		$lastInsertID = $this->conn->lastInsertId();
+		 	$username = $this->getUserNameFromID($userid);
+			$sql = $this->conn->prepare("INSERT INTO photo (photo_image, photo_text, photo_header, photo_user)
+									 VALUES (:image, :description, :title, :username)");
+			$sql->bindparam('image', $image);
+			$sql->bindparam('description', $description);
+			$sql->bindparam('title', $title);
+			$sql->bindparam('username', $username);
+			$sql->execute();
+			$lastInsertID = $this->conn->lastInsertId();
 
 		return $lastInsertID;
    }
 
    public function selectPictures($userID) {
-     $sql   = "SELECT login_username FROM login WHERE login_id = :userID";
-     $stmt  = $this->conn->prepare($sql);
-     $stmt->bindParam('userID', $userID);
+		 $username 	= $this->getUserNameFromId($userID);
+		 $sql   		= "SELECT photo_id, photo_header, photo_text, photo_image FROM photo WHERE photo_user = :username";
+     $stmt  		= $this->conn->prepare($sql);
+     $stmt->bindParam('username', $username);
      $stmt->execute();
 		 $stmt->setFetchMode(PDO::FETCH_ASSOC);
-		 $result = $stmt->fetch();
+		 $result 		= $stmt->fetchAll();
 
-		 $username = $result['login_username'];
- 		return $username;
+			return $result;
    }
 }
