a68f1df4af3d14d5a16343ef5b1dff9b6267beac
BootySwooty <petjon_real@hotmail.com>
2019-05-04 01:22:37 +0200

    Should work test does

---
+++ petch16/mvc/public/app/models/Post.php
@@ -0,0 +1,91 @@
+<?php
+
+
+    class Post
+    {
+       private $db;
+
+       public function __construct()
+       {
+           $this->db = new Database();
+       }
+
+       public function getPosts()
+       {
+           $this->db->query('select p.id as post_id, p.user_id, u.name, u.email, p.title, p.body, p.created_at, p.picture 
+                                 from posts p 
+                                 left join users u on u.id = p.user_id 
+                                 order by p.created_at desc');
+           return $this->db->resultSet();
+
+       }
+
+       public function getPostById($id)
+       {
+           $this->db->query('select * from posts where id = :id');
+           $this->db->bind(':id',$id);
+           return $this->db->single();
+       }
+    
+        public function getPostByUserId($user_id)
+        {
+            $this->db->query('select count(*) as total from posts where user_id = :user_id');
+            $this->db->bind(':user_id',$user_id);
+            return $this->db->single();
+        }
+
+        public function getPostFromUserId($user_id)
+        {
+            $this->db->query('select * from posts where user_id = :user_id');
+            $this->db->bind(':user_id',$user_id);
+            return $this->db->resultSet();
+        }
+       
+       public function addPost($data)
+       {
+           $this->db->query('INSERT INTO posts (user_id, title, body, picture) values (:user_id, :title, :body, :picture)');
+           // Bind values
+           $this->db->bind(':user_id', $data['user_id']);
+           $this->db->bind(':title', $data['title']);
+           $this->db->bind(':body', $data['body']);
+           $this->db->bind(':picture', $data['picture']);
+
+
+           // Execute
+           if( $this->db->execute() ){
+               return true;
+           } else {
+               return false;
+           }
+       }
+
+       public function updatePost($data)
+       {
+           $this->db->query('UPDATE posts SET title = :title, body = :body where id = :id');
+           // Bind values
+           $this->db->bind(':id', $data['id']);
+           $this->db->bind(':title', $data['title']);
+           $this->db->bind(':body', $data['body']);
+
+           // Execute
+           if( $this->db->execute() ){
+               return true;
+           } else {
+               return false;
+           }
+       }
+
+       public function deletePost($id)
+       {
+           $this->db->query('DELETE FROM posts where id = :id');
+           // Bind values
+           $this->db->bind(':id', $id);
+
+           // Execute
+           if( $this->db->execute() ){
+               return true;
+           } else {
+               return false;
+           }
+       }
+    }
